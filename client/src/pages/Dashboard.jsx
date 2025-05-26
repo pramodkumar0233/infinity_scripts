b8417994979b221b0254b40f43da53f6d15eba91
pramodkumar0233 <bhumandla849@gmail.com>
2025-05-26 14:19:11 +0530

    till text summarizer is done

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -16,28 +16,23 @@ export default function Dashboard() {
         </div>
 
         <div className="card" onClick={() => navigate("/text-from-image")}>
-          <img src="/images/text-from-image.jpg" alt="OCR" />
+          <img src="/images/tfi.png" alt="OCR" />
           <h3>Text from Image</h3>
         </div>
 
         <div className="card" onClick={() => navigate("/text-to-speech")}>
-          <img src="/images/tts.jpg" alt="Text to Speech" />
+          <img src="/images/text-to-speech.png" alt="Text to Speech" />
           <h3>Text to Speech</h3>
         </div>
 
         <div className="card" onClick={() => navigate("/speech-to-text")}>
-          <img src="/images/tts.jpg" alt="Speech to text" />
+          <img src="/images/stt.png" alt="Speech to text" />
           <h3>Speech to Text</h3>
         </div>
 
         <div className="card" onClick={() => navigate("/summarize")}>
-          <img src="/images/tts.jpg" alt="summarize" />
+          <img src="/images/tx.webp" alt="summarize" />
           <h3>Summarize</h3>
-        </div>
-
-        <div className="card" onClick={() => navigate("/future funtions")}>
-          <img src="/images/translate.jpg" alt="IDK" />
-          <h3>IDK</h3>
         </div>
 
       </div>
